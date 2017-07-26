20dda001ae00b201592950458ddb770510fb802c
Qilei Ren <renqilei@gmail.com>
2017-07-26 16:18:19 +0800

    新增文章分享功能，小幅修改前端
    * 支持文章展示页的中进行文章分享
    * 小幅修改了前端展示

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,3 +1,5 @@
+{{-- articles.show --}}
+
 @extends("layouts.main")
 
 @section("main-body")
@@ -65,6 +67,25 @@
                     {!! $article['content'] !!}
                 </div>
 
+                <div id="article-show-share">
+
+                </div>
+
+                <script type="text/javascript">
+                    var $config = {
+                        // url                 : '', // 网址，默认使用 window.location.href
+                        source              : '<meta name="site" content="http://devops-club.com" />', // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
+                        // title               : '', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
+                        // description         : '', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
+                        // image               : '', // 图片, 默认取网页中第一个img标签
+                        sites               : ['qq', 'wechat', 'weibo', 'qzone', 'tencent', 'douban', 'diandian', 'linkedin', 'facebook', 'twitter', 'google'], // 启用的站点
+                        disabled            : [], // 禁用的站点
+                        wechatQrcodeTitle   : "微信扫一扫：分享", // 微信二维码提示文字
+                        wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>',
+                    };
+                    $("#article-show-share").share($config);
+                </script>
+
             </div>
 
             {{-- 手机版显示方式还未确定 --}}
@@ -76,4 +97,9 @@
 
         </div>
     </div>
+@endsection
+
+@section("main-head")
+    <link href="//cdn.bootcss.com/social-share.js/1.0.16/css/share.min.css" rel="stylesheet">
+    <script src="//cdn.bootcss.com/social-share.js/1.0.16/js/jquery.share.min.js"></script>
 @endsection
